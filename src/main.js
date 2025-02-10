05de2a666774106e4e2040d7de7f167c5d6564cf
DELILLE Elone <elone.delille@algosup.com>
2025-02-10 17:29:10 +0100

    Edit

---
+++ src/main.js
@@ -0,0 +1,51 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import Layout from './components/Layout.vue'
+import HomePage from './components/HomePage.vue'
+import IncidentDetail from './components/IncidentDetail.vue'
+import MaintenanceDetail from './components/MaintenanceDetail.vue'
+import IncidentsPage from './components/IncidentsPage.vue'
+import MaintenancePage from './components/MaintenancePage.vue'
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/',
+      component: Layout,
+      children: [
+        {
+          path: '',
+          component: HomePage,
+          name: 'home'
+        },
+        {
+          path: 'incidents',
+          component: IncidentsPage,
+          name: 'incidents'
+        },
+        {
+          path: 'maintenance',
+          component: MaintenancePage,
+          name: 'maintenance'
+        },
+        {
+          path: 'incident/:id',
+          component: IncidentDetail,
+          name: 'incident-detail'
+        },
+        {
+          path: 'maintenance/:id',
+          component: MaintenanceDetail,
+          name: 'maintenance-detail'
+        }
+      ]
+    }
+  ]
+})
+
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
